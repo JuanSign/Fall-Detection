ef97f87a6e2d5b74211df715a069a2783dae1140
Juan Siagian <jsa.siagian@gmail.com>
2024-12-25 14:16:10 +0700

    feat : implementing pipeline model

--- src/pose-estimation/MediaPipe.py
+++ src/pose-estimation/MediaPipe.py
@@ -1,28 +1,32 @@
 import cv2
 import mediapipe as mp
 
+# definition
+class MediaPipe_Pipeline:
+    def __init__(self, pose, mp_drawing):
+        self.pose = pose
+        self.mp_drawing = mp_drawing
+    
+    @staticmethod
+    def process_image(path):
+        image = cv2.imread(path)
+        rgb_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(rgb_img)        
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        cv2.imshow("Pose Estimation", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+# example usage
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(
     min_detection_confidence=0.5, 
     min_tracking_confidence=0.5,
     model_complexity=2 
 )
-
 mp_drawing = mp.solutions.drawing_utils
 
-path = r'data\train\subject-1\fall\1_sitting_falls\frame008.jpg'
-image = cv2.imread(path)
-
-image_height, image_width, _ = image.shape
-
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-results = pose.process(rgb_image)
-
-if results.pose_landmarks:
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-cv2.imshow("Pose Estimation", image)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+pipeline = MediaPipe_Pipeline(pose, mp_drawing)
+pipeline.process_image(r'PATH')
